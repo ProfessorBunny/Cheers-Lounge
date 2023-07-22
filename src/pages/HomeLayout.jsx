4e9ad62445d8b98f6bd86c9a62f63955533b4943
200~ProfessorBunny <nikunjraghav24@gmail.com>
2023-07-22 11:05:06 +0100

    Linking components

--- src/pages/HomeLayout.jsx
+++ src/pages/HomeLayout.jsx
@@ -1,4 +1,11 @@
+import { Link } from "react-router-dom";
+
 const HomeLayout = () => {
-  return <div>HomeLayout</div>;
+  return (
+    <div>
+      <h2>HomeLayout</h2>
+      <Link to="/about">About Page</Link>
+    </div>
+  );
 };
 export default HomeLayout;
