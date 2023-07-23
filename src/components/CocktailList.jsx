d72ee8f86574dece3263cf5a957ca23fdcee3fa9
200~ProfessorBunny <nikunjraghav24@gmail.com>
2023-07-23 13:36:17 +0100

    formatted cokatil list passed to card component

--- src/components/CocktailList.jsx
+++ src/components/CocktailList.jsx
@@ -1,7 +1,25 @@
+import Wrapper from "../assets/wrappers/CocktailList";
+import CocktailCard from "./CocktailCard";
 const CocktailList = ({ drinks }) => {
   if (!drinks) {
     return <h4 style={{ textAlign: "center" }}>No Drinks found :(</h4>;
   }
-  return <div>CocktailList</div>;
+  const formattedDrinks = drinks.map((drink) => {
+    const { idDrink, strAlcoholic, strDrinkThumb, strGlass, strDrink } = drink;
+    return {
+      id: idDrink,
+      name: strDrink,
+      info: strAlcoholic,
+      glass: strGlass,
+      image: strDrinkThumb,
+    };
+  });
+  return (
+    <Wrapper>
+      {formattedDrinks.map((item) => {
+        return <CocktailCard key={item.id} {...item} />;
+      })}
+    </Wrapper>
+  );
 };
 export default CocktailList;
