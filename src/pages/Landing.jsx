88f98f23ef6d5a35034c3d3b730969487fc55840
200~ProfessorBunny <nikunjraghav24@gmail.com>
2023-07-23 12:54:22 +0100

    Fetching drinks from API

--- src/pages/Landing.jsx
+++ src/pages/Landing.jsx
@@ -1,16 +1,20 @@
 import axios from "axios";
 import { useLoaderData } from "react-router-dom";
+
 const cocktailSearchUrl =
   "https://www.thecocktaildb.com/api/json/v1/1/search.php?s=";
-export const loader = async () => {
-  const searchRecipe = "Mojito";
-  const response = await axios.get(`${cocktailSearchUrl}${searchRecipe}`);
 
-  return response;
+export const loader = async () => {
+  const saearchedDrink = "Mojito";
+  const response = await axios.get(`${cocktailSearchUrl}${saearchedDrink}`);
+
+  return { drinks: response.data.drinks, saearchedDrink };
 };
+
 const Landing = () => {
-  const data = useLoaderData();
-  console.log(data);
+  const { drinks, searchedDrink } = useLoaderData();
+  console.log(drinks);
+  console.log(searchedDrink);
   return <h2>Landing</h2>;
 };
 export default Landing;
