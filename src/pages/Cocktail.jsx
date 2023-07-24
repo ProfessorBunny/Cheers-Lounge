3e68d1ee204054f7d1b08ffbbcb13896804c08ec
200~ProfessorBunny <nikunjraghav24@gmail.com>
2023-07-24 14:42:36 +0100

    Displaying data for single cocktail page

--- src/pages/Cocktail.jsx
+++ src/pages/Cocktail.jsx
@@ -21,6 +21,40 @@ const Cocktail = () => {
     strInstructions: instructions,
     strAlcoholic: info,
   } = dataForSingleDrink;
-  return <Wrapper>Cocktail</Wrapper>;
+  return (
+    <Wrapper>
+      <header>
+        <Link to="/" className="btn">
+          Home
+        </Link>
+        <h3>{name}</h3>
+      </header>
+      <div className="drink">
+        <img src={image} alt={name} className="img" />
+        <div className="drink-info">
+          <p>
+            <span className="drink-data">Name:</span>
+            {name}
+          </p>
+          <p>
+            <span className="drink-data">Category:</span>
+            {category}
+          </p>
+          <p>
+            <span className="drink-data">Glass:</span>
+            {glass}
+          </p>
+          <p>
+            <span className="drink-data">Type:</span>
+            {info}
+          </p>
+          <p>
+            <span className="drink-data">Instructions:</span>
+            {instructions}
+          </p>
+        </div>
+      </div>
+    </Wrapper>
+  );
 };
 export default Cocktail;
