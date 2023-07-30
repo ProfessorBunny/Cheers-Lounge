9b01fce63fd96d1c27aba3e67c75d6f14f80e23b
200~ProfessorBunny <nikunjraghav24@gmail.com>
2023-07-30 18:13:10 +0100

    Setting up react query client with stale time

--- src/App.jsx
+++ src/App.jsx
@@ -11,6 +11,16 @@ import {
 import { loader as landingLoader } from "./pages/Landing";
 import { loader as singleCocktailoader } from "./pages/Cocktail";
 import { action as newsLetterAction } from "./pages/NewsLetter";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      staleTime: 1000 * 60 * 8,
+    },
+  },
+});
 const router = createBrowserRouter([
   {
     path: "/",
@@ -42,6 +52,11 @@ const router = createBrowserRouter([
   },
 ]);
 const App = () => {
-  return <RouterProvider router={router} />;
+  return (
+    <QueryClientProvider>
+      <RouterProvider router={router} />;
+      {/* <ReactQueryDevtools initialIsOpen={false} /> */}
+    </QueryClientProvider>
+  );
 };
 export default App;
