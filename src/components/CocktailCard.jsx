4bb588902aa2a6b9761cf310624622e53d025f19
ProfessorBuny <nikunjraghav24@gmail.com>
2025-01-25 21:13:56 +0000

    update

--- src/components/CocktailCard.jsx
+++ src/components/CocktailCard.jsx
@@ -12,7 +12,7 @@ const CocktailCard = ({ id, name, info, glass, image }) => {
         <p>{info}</p>
         <Link to={`/cocktail/${id}`} className="btn">
           {" "}
-          Details
+          Detail
         </Link>
       </div>
     </Wrapper>
